1d747f0045cb159d70d1f27446488fd8a82536c8
Vlad <stoicamihaivlad@gmail.com>
2020-02-20 11:16:08 +0200

    Before All Blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog model
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
